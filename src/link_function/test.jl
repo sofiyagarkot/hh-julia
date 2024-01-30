383c01026e7df72c703a79fe5f2904b2c308947f
sofiagarkot <sofia.garkot2001@gmail.com>
2024-01-30 18:13:47 +0100

    Link functions for Hodkin-Huxley.

--- src/link_function/test.jl
+++ src/link_function/test.jl
@@ -1,5 +1,6 @@
 include("../utils.jl")
 using Plots
+using DifferentialEquations
 
 SMOOTH = DENSE = false
 ADAPTIVE = false
@@ -8,13 +9,14 @@ TO_SAVE = false
 # fixed diffusion
 DM = FixedDiffusion()
 
-prob = define_problem()
+# prob = define_problem()
 prob = define_problem_with_link_function()
 evaltspan = (0.0, 1.0)
 tstops = range(evaltspan..., length=10000)
 
-algorithm = [EK1(prior=IWP(3), smooth=SMOOTH, diffusionmodel=DM)]
-names = ["EK1(IWP(3))"]
+# algorithm = [EK1(prior=IWP(3), smooth=SMOOTH, diffusionmodel=DM)]
+
+algorithms = [BS3()]
 
 solution = solve(prob, algorithms[1], tstops=tstops, adaptive=ADAPTIVE, dense=DENSE)
 
