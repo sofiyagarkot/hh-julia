9675285931e263dffd6874e33ad2d2d24c184b84
sofiagarkot <sofia.garkot2001@gmail.com>
2024-02-06 11:06:53 +0100

    Difference in error estimate for prob solvers.

---
+++ src/wp-diagrams-l2/dt-l2-error.jl
@@ -0,0 +1,85 @@
+using RecursiveArrayTools
+include("../utils.jl")
+
+dts = 10.0 .^ range(-2, -4, length=5)
+evaltspan = (0.0, 50.0)
+
+DENSE = SMOOTH = TO_SAVE = false
+ADAPTIVE = false
+SAVE_EVERYSTEP = false
+
+DM = FixedDiffusion()
+
+_setups = [
+    # "Implicit Euler" => Dict(:alg=>ImplicitEuler())
+    "Exponential Euler" => Dict(:alg=>LawsonEuler(krylov = true, m = 50)) # on krylov and m : https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/ 
+    "EK1(1)" => Dict(:alg=>EK1(order=1, smooth=DENSE, diffusionmodel=DM))
+    "EK1(2)" => Dict(:alg=>EK1(order=2, smooth=DENSE, diffusionmodel=DM))
+    "EK1(3)" => Dict(:alg=>EK1(order=3, smooth=DENSE, diffusionmodel=DM))
+    "EK1(4)" => Dict(:alg=>EK1(order=4, smooth=DENSE, diffusionmodel=DM))
+    ]
+    
+labels = first.(_setups)
+setups = last.(_setups)
+
+# COLORS
+colors = [colorant"darkorange"]
+# push!(colors, colorant"purple")
+
+c1 = colorant"lightblue"
+c2 = colorant"darkblue"
+colors2 = range(c1, stop=c2, length=4)
+
+for c in colors2
+    push!(colors, c)
+end
+
+# L2 loss from https://github.com/SciML/DiffEqDevTools.jl/blob/master/src/test_solution.jl 
+function l2(sol, analytic)
+    return sqrt(recursive_mean(vecvecapply((x) -> float(x) .^ 2,
+                                                      sol - analytic)))
+end
+
+prob = define_problem()
+
+
+algorithms = [setup[:alg] for setup in setups]
+
+p = plot(legendfont = font(7), size = (500, 400))
+
+for i in 1:length(algorithms)
+    print("i: ", i, "\n")
+    algorithm = algorithms[i]
+    errors = []
+
+    for dt in dts
+        print("dt", dt, "\n")
+        # tstops = range(evaltspan..., length=Int(round(evaltspan[end]/dt, digits = 0)))
+        # print("tstops: ", tstops, "\n")
+        solution = solve(prob, algorithm, dt=dt, dense = false)
+        reference = solve(prob, Vern9(), abstol=1e-9, reltol=1e-9, saveat=solution.t)
+        error = l2(solution.u, reference(solution.t))
+        push!(errors, error)
+    
+    end
+    
+    plot!(p, dts, errors, label=labels[i], 
+        framestyle=:axes,
+        xaxis=:log10, yaxis=:log10,
+        color=colors[i],
+        fg_legend = :transparent)
+
+    scatter!(p, dts, errors, label="", 
+        framestyle=:axes,
+        xaxis=:log10, yaxis=:log10,
+        color=colors[i],
+        fg_legend = :transparent)
+    display(p)
+end
+
+plot!(p, xaxis=:log10, yaxis=:log10, 
+        legend=:bottomright, xlabel="dt", 
+        ylabel="L2 error")
+p
+
+# savefig(p, "./visuals/my-wp-diagram.png")
